2677d209b933bde818e8889853d64e7bfbe4a0f8
[CloeDelage2301] <[cloe.delage1@etu.unilim.fr]>
2025-10-22 14:38:02 +0200

    sae3.01

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/Sae3.01_Cloe_Delage",
+  base: "Sae3.01_Cloe_Delage",
 });
